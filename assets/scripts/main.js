015eb3732680b85c393ce6bfd97df38ba3876735
El Batrak <ali.hammoud.lb@gmail.com>
2019-10-08 17:19:36 +0300

    "Marker lost" warning, UI cleanup, touch calibration

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,37 +1,41 @@
 window.addEventListener('load', function (e) {
   var hammertime = new Hammer(document);
+  var markerMsg = document.querySelector('#m-marker-lost');
 
   var marker = document.querySelector('#qr-marker');
   marker.addEventListener("markerFound", function () {
     var markerId = marker.id;
     var model = document.querySelector('#model');
-
     console.log('Marker Found', markerId);
 
     hammertime.on('pan', function (ev) {
-      console.log('panning ' + ev.deltaX + ', ' + ev.deltaY);
       
-      var rot_speed = 1.5
+      var rot_speed = 0.8
       
       switch(ev.direction){
           case 2: //left
-          model.object3D.rotation.z -= THREE.Math.degToRad(rot_speed * 2)
-          break;
-          
-          case 4: //right
-          model.object3D.rotation.z += THREE.Math.degToRad(rot_speed * 2)
-          break;
-          
-          case 8: //up
           model.object3D.rotation.x += THREE.Math.degToRad(rot_speed)
           break;
           
-          case 16: //down
+          case 4: //right
           model.object3D.rotation.x -= THREE.Math.degToRad(rot_speed)
           break;
+          
       }
       
     });
+    
+    if (!markerMsg.classList.contains('hidden'))
+      markerMsg.classList.toggle('hidden');
 
   });
+  
+  marker.addEventListener("markerLost", function () {
+    var markerId = marker.id;
+    console.log('Marker Lost', markerId);
+    
+    markerMsg.classList.toggle('hidden');
+    
+  });
+  
 })
