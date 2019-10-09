b1b25df378916d023f2c7aca43b074913a7ff65c
El Batrak <ali.hammoud.lb@gmail.com>
2019-10-09 16:18:41 +0300

    Added backdrop and mock GUI

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,16 +1,26 @@
 window.addEventListener('load', function (e) {
   var hammertime = new Hammer(document);
   var markerMsg = document.querySelector('#m-marker-lost');
-
+  var finderGfx = document.querySelector('.finder');
+  var turntableGfx = document.querySelector('#turntable');
+  var bottomMenu = document.querySelector('#bottom-menu');
+  var backdrop = document.querySelector('#backdrop');
+  
   var marker = document.querySelector('#qr-marker');
   marker.addEventListener("markerFound", function () {
     var markerId = marker.id;
     var model = document.querySelector('#model');
     console.log('Marker Found', markerId);
+    
+    markerMsg.classList.add('hidden');
+    finderGfx.classList.add('fade-out');
+    turntableGfx.classList.remove('fade-out');
+    bottomMenu.classList.add('slide-bottom');
+    backdrop.classList.remove('hide');
 
     hammertime.on('pan', function (ev) {
-      
-      var rot_speed = 0.8
+      turntableGfx.classList.add('fade-out');
+      var rot_speed = 0.8;
       
       switch(ev.direction){
           case 2: //left
@@ -24,9 +34,6 @@ window.addEventListener('load', function (e) {
       }
       
     });
-    
-    if (!markerMsg.classList.contains('hidden'))
-      markerMsg.classList.toggle('hidden');
 
   });
   
@@ -34,7 +41,11 @@ window.addEventListener('load', function (e) {
     var markerId = marker.id;
     console.log('Marker Lost', markerId);
     
-    markerMsg.classList.toggle('hidden');
+    markerMsg.classList.remove('hidden');
+    finderGfx.classList.remove('fade-out');
+    turntableGfx.classList.add('fade-out');
+    bottomMenu.classList.remove('slide-bottom');
+    backdrop.classList.add('hide');
     
   });
   
