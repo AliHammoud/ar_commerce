af8c5e07a5f77b42545118e3d4f16585699b1d0f
El Batrak <ali.hammoud.lb@gmail.com>
2019-11-05 01:53:14 -0800

    Tweaked paths

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,4 @@
 window.addEventListener('load', function (e) {
-  AFRAME.registerComponent('changeTex', {
-    init: function(){alert('hey')}
-  });
   document.querySelector('a-scene').addEventListener('loaded', function (e) {
     document.querySelector('#backdrop').classList.toggle('hide');
     document.querySelector('#bottom-menu').classList.toggle('slide-bottom');
