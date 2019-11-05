f861fbfc9ae6b317ae289220f6d04cfde0d5c75e
El Batrak <ali.hammoud.lb@gmail.com>
2019-11-05 01:39:37 -0800

    Added customization scenario

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,11 @@
 window.addEventListener('load', function (e) {
+  AFRAME.registerComponent('changeTex', {
+    init: function(){alert('hey')}
+  });
   document.querySelector('a-scene').addEventListener('loaded', function (e) {
     document.querySelector('#backdrop').classList.toggle('hide');
     document.querySelector('#bottom-menu').classList.toggle('slide-bottom');
+    
+    var shoe = document.querySelector("#target");
   })
 })
